f43c63379fda44925abc3acd9042e653f40fd1a2
Rasmus Porsager <rasmus@porsager.com>
2018-02-21 14:42:54 +0100

    Make fully async

--- index.js
+++ index.js
@@ -3,42 +3,49 @@ const https = require('https')
     , URL = require('url')
 
 function download(url, dest, progress) {
-  const stat = fs.existsSync(dest) && fs.statSync(dest)
-      , start = stat ? stat.size : 0
-
   return new Promise((resolve, reject) => {
-    const file = fs.createWriteStream(dest, {
-      flags: start ? 'r+' : 'w',
-      start
-    })
+    fs.stat(dest, (err, stat) => {
+      if (err && err.code !== 'ENOENT')
+        return reject(err)
 
-    const req = https.request(Object.assign({
-      method: 'GET',
-      headers: { Range: 'bytes=' + start + '-' }
-    }, URL.parse(url)), res => {
-      const length = parseInt(res.headers['content-length'], 10)
+      let start = stat ? stat.size : 0
 
-      let downloaded = start
+      const req = https.request(Object.assign({
+        method: 'GET',
+        headers: { Range: 'bytes=' + start + '-' }
+      }, URL.parse(url)), res => {
+        if (!res.headers['content-range'])
+          start = 0
 
-      res.on('data', chunk => {
-        downloaded += chunk.length
-        typeof progress === 'function' && progress(downloaded, (length + start))
-        file.write(chunk)
+        const file = fs.createWriteStream(dest, {
+          flags: start ? 'r+' : 'w',
+          start
+        })
+
+        file.on('error', e => {
+          req.abort()
+          reject(e)
+        })
+
+        const length = parseInt(res.headers['content-length'], 10)
+
+        let downloaded = start
+
+        res.on('data', chunk => {
+          downloaded += chunk.length
+          typeof progress === 'function' && progress(downloaded, (length + start))
+          file.write(chunk)
+        })
+
+        res.on('end', resolve)
       })
 
-      res.on('end', resolve)
+      req.on('error', reject)
+      req.on('close', reject)
+      req.on('timeout', () => { req.abort() })
+      req.setTimeout(30 * 1000)
+      req.end()
     })
-
-    file.on('error', e => {
-      req.abort()
-      reject(e)
-    })
-
-    req.on('error', reject)
-    req.on('close', reject)
-    req.on('timeout', () => { req.abort() })
-    req.setTimeout(30 * 1000)
-    req.end()
   })
 }
 
